08814a362fc597bff73e422adc7d4ec6bf2e747c
Aayank Singhai <aayanksinghai02@gmail.com>
2020-03-21 16:21:32 +0530

    Packaged with WAR and TOMCAT 9.0 Configured

--- conference/src/main/java/com/pluralsight/conference/ConferenceApplication.java
+++ conference/src/main/java/com/pluralsight/conference/ConferenceApplication.java
@@ -2,10 +2,16 @@ package com.pluralsight.conference;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class ConferenceApplication extends SpringBootServletInitializer{
+	
+	  @Override
+	   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	      return application.sources(ConferenceApplication.class);
+	   }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConferenceApplication.class, args);
