e0c2adfd462687bcd8740adb10ef801240227c41
Aayank Singhai <aayanksinghai02@gmail.com>
2020-03-21 18:08:37 +0530

    RESTAPI all METHODS

---
+++ RESTAPI/src/main/java/com/demo/RESTAPI/model/Product.java
@@ -0,0 +1,21 @@
+package com.demo.RESTAPI.model;
+
+public class Product {
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	private String id;
+	
+	
+
+}
