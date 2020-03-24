b182793a4b9c4e75edc31799558ff76a392956cb
Aayank Singhai <aayanksinghai02@gmail.com>
2020-03-25 00:15:57 +0530

    Spring REST Data

---
+++ dataREST/src/main/java/com/example/dataREST/DataRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.dataREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataRestApplication.class, args);
+	}
+
+}
---
+++ dataREST/src/test/java/com/example/dataREST/DataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dataREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
